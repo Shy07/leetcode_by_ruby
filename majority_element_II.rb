21d694da5f78101340b67957816982628865a336
Lynch HSU <Gernischt@gmail.com>
2015-08-19 19:53:07 +0800

    update

--- majority_element_II.rb
+++ majority_element_II.rb
@@ -1,5 +1,4 @@
 # fast and simple
 def majority_element(nums)
-  a = nums.sort.chunk{ |x| x }.map &:last
-  a.inject([]) {|mem, var| mem << var[0] if var.size > nums.size / 3; mem }
+  nums.sort.chunk{|x|x}.map(&:last).inject([]){|m,v| m << v[0] if v.size > nums.size / 3;m}
 end
